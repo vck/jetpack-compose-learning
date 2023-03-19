57244c796c70a78149720cca14876b8a0f384f30
dasta <vickydasta@gmail.com>
2023-03-19 19:58:48 +0700

    add initial working todo-ish app

--- ListMe/app/src/main/java/com/vicky/listme/MainActivity.kt
+++ ListMe/app/src/main/java/com/vicky/listme/MainActivity.kt
@@ -3,44 +3,55 @@ package com.vicky.listme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.vicky.listme.ui.theme.ListMeTheme
+
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContent{
             ListMeTheme {
-                var count = remember {
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
                 }
 
+                var names by remember {
+                    mutableStateOf( listOf<String>() )
+                }
                 Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Text(
-                        text = count.value.toString(),
-                        fontSize = 30.sp
-                    )
-                    Button(onClick = {
-                        count.value++
-                    }) {
-                        Text(text = "Click me! $count")
+                    Row (
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(value = name, onValueChange = { text ->
+                            name = text
+                        })
+                        Button (onClick = {
+                            names = names + name
+                            name = ""
+                        }){
+                            Text(text = "Add")
+                        }
+                    }
+
+                    LazyColumn {
+                        items(names){ currentName ->
+                            Text(text = currentName)
+                        }
                     }
                 }
             }
